6a1a90e84dfbb3c7d8b66ae60ddee79e8468b5fd
Isabelle Carminati <ISABELLE.MCARMINATI@gmail.com>
2025-03-17 16:43:25 -0400

    Navbar outline

---
+++ PinkBox/src/navbar/Wishlist.jsx
@@ -0,0 +1,10 @@
+//import React from 'react'
+
+export default function Home()
+{
+    return(
+        <div>
+            <h3 style={{ textAlign: 'center'}}>Wishlist Page</h3>
+        </div>
+    )
+}
