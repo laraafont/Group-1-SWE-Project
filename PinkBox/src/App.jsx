123d05041c95eb9f6f8e5435b27820dd710e589d
Isabelle Carminati <ISABELLE.MCARMINATI@gmail.com>
2025-03-17 17:08:36 -0400

    Linked Pages

--- PinkBox/src/App.jsx
+++ PinkBox/src/App.jsx
@@ -1,12 +1,20 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./navbar/NavBar"; // Correct import from src/navbar
 
+function App() {
   return (
-    <div>
-      <h2 style={{ textAlign: 'center', fontFamily: 'Helvetica', fontSize: '1.5rem', color: '#333' }}>
-        React Navigation Bar
-      </h2>
-    </div>
-  )
+    <BrowserRouter>
+      {/* Navigation Bar */}
+      <NavBar />
+
+      {/* Main content */}
+      <div>
+        <h2 style={{ textAlign: 'center', fontFamily: 'Helvetica', fontSize: '1.5rem', color: '#333' }}>
+          React Navigation Bar
+        </h2>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- PinkBox/src/navbar/Navbar.jsx
+++ PinkBox/src/navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-//import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Movies from './Movies';
+import Login from './Login';
+import Cart from './Cart';
+import Wishlist from './Wishlist';
 
-export default function NavBar()
-{
-    return(
+export default function NavBar() {
+    return (
         <div>
-            NavBar
+            {/* Navigation Links */}
+            <nav style={{ display: 'flex', gap: '20px', padding: '10px' }}>
+                <Link to="/">Home</Link> {/* Default Page */}
+                <Link to="/movies">Movies</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/cart">Cart</Link>
+                <Link to="/wishlist">Wishlist</Link>
+            </nav>
+
+            {/* Route Definitions */}
+            <Routes>
+                <Route path="/" element={<Home />} /> {/* Default Page */}
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/wishlist" element={<Wishlist />} />
+            </Routes>
         </div>
-    )
+    );
 }
