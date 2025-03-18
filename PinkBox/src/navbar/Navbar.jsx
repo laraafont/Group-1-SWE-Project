01d7421a0a23fc159064486548b4d7c04af1abc9
Isabelle Carminati <ISABELLE.MCARMINATI@gmail.com>
2025-03-18 00:07:10 -0400

    Made website icon visible

--- PinkBox/src/navbar/Navbar.jsx
+++ PinkBox/src/navbar/Navbar.jsx
@@ -5,11 +5,14 @@ import Login from './Login';
 import Cart from './Cart';
 import Wishlist from './Wishlist';
 
+import './navbar.css';
+
 export default function NavBar() {
     return (
         <div>
             {/* Navigation Links */}
             <nav style={{ display: 'flex', gap: '20px', padding: '10px' }}>
+                {/* Adds actual links to navbar */}
                 <Link to="/">Home</Link> {/* Default Page */}
                 <Link to="/movies">Movies</Link>
                 <Link to="/login">Login</Link>
@@ -19,6 +22,7 @@ export default function NavBar() {
 
             {/* Route Definitions */}
             <Routes>
+                {/* Links to our .jsx files within navbar */}
                 <Route path="/" element={<Home />} /> {/* Default Page */}
                 <Route path="/movies" element={<Movies />} />
                 <Route path="/login" element={<Login />} />
